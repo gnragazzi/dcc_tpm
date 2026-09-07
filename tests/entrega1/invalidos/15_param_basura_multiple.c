948f4f12de19b57c69d26efecddf16cc537f83d4
gnragazzi <gnragazzi@gmail.com>
2026-09-07 03:14:05 -0300

    fix 6

---
+++ tests/entrega1/invalidos/15_param_basura_multiple.c
@@ -0,0 +1,10 @@
+# Expectativa: Error 41: Simbolo inesperado o falta especificador de tipo #
+# Prueba: Varios simbolos invalidos consecutivos antes del primer tipo #
+
+void f(* * * int x)
+{
+}
+
+void main()
+{
+}
---
+++ tests/entrega1/invalidos/15_param_parentesis_extra.c
@@ -0,0 +1,10 @@
+# Expectativa: Error 49: Simbolo inesperado o falta simb. al comienzo de proposicion compuesta #
+# Prueba: Parentesis de cierre duplicado tras la lista de parametros #
+
+void f(int x))
+{
+}
+
+void main()
+{
+}
