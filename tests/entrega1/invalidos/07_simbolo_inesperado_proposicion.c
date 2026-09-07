ea341aca2f6c9e0875029b169f434dcd74fd9d08
gnragazzi <gnragazzi@gmail.com>
2026-09-07 02:24:03 -0300

    fix 4

--- tests/entrega1/invalidos/07_simbolo_inesperado_proposicion.c
+++ tests/entrega1/invalidos/07_simbolo_inesperado_proposicion.c
@@ -1,4 +1,4 @@
-# Expectativa: Error 52: Simbolo inesperado o falta simb. al comienzo de proposicion #
+# Expectativa: Error 51: Simbolo inesperado despues de declaracion #
 # Prueba: Operador binario al inicio de proposicion en bloque compuesto #
 
 void main()
