24b1fac77e73f70f382428b111019bf47529777c
gnragazzi <gnragazzi@gmail.com>
2026-09-06 22:00:12 -0300

    feat: agregar unidad de traducción, declaración y especificador de tipo

--- src/error.c
+++ src/error.c
@@ -47,6 +47,7 @@ void error_print(int ne)
 		case 35: printf("\t Error %d: Falta [\n", ne); break;
 		/* posibles errores personalizados (que no sean parafrasis de errores ya existentes) 36-39*/
 		case 36: printf("\t Error %d: folset mal formado.\n", ne); break;
+		case 37: printf("\t Error %d: Simbolo inesperado o falta simb. al comienzo del archivo fuente", ne); break;
 		/* errores de recuperacion de errores */
 		case 40: printf("\t Error %d: Al inicio del programa\n", ne); break;
 		case 41: printf("\t Error %d: Simbolo inesperado o falta especificador de tipo\n", ne); break;
