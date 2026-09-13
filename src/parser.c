a81d3cb30f4efa8b5d5b52a0e806d575f49d24d4
gnragazzi <gnragazzi@gmail.com>
2026-09-13 11:08:48 -0300

    fix 5, caso trivial

--- src/parser.c
+++ src/parser.c
@@ -231,9 +231,6 @@ void proposicion_compuesta(set folset)
 {
 	test(F_PROPOSICION_COMPUESTA, folset | F_LISTA_DECLARACIONES | F_LISTA_PROPOSICIONES | CLLA_CIE, 49);
 
-	if(!lookahead_in(F_PROPOSICION_COMPUESTA | F_LISTA_DECLARACIONES | F_LISTA_PROPOSICIONES | CLLA_CIE))
-		return;
-
 	if(lookahead_in(CLLA_ABR))
 		scanner();
 
