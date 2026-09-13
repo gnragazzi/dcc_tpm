69ea79cc01ac525bbf738d90dfbe9c5ef1503cc3
gnragazzi <gnragazzi@gmail.com>
2026-09-13 10:41:29 -0300

    fix 4

--- tests/entrega1/invalidos/16_bloque_simbolo_inesperado.c
+++ tests/entrega1/invalidos/16_bloque_simbolo_inesperado.c
@@ -1,7 +1,7 @@
-# Expectativa: Error 52: Simbolo inesperado o falta simb. al comienzo de proposicion #
-# Prueba: operador binario al comienzo del cuerpo de un bloque. Fija el test intermedio de #
-# proposicion_compuesta: sin el, los dos guardianes se saltean en silencio y el error lo #
-# reporta match(CLLA_CIE, 25) como "falta }", sin consumir. #
+# Expectativa: Error 25 (Falta }) seguido de Error 50 #
+# Prueba: operador binario al comienzo del cuerpo de un bloque. Devolución punto 4: sin el test #
+# intermedio de proposicion_compuesta (eliminado), los dos guardianes se saltean en silencio y el #
+# error lo reporta match(CLLA_CIE, 25) como "falta }", sin consumir; el test final agrega el 50. #
 
 void main()
 {
